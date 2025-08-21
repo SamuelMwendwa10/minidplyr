3a52ecc73c8b99f5a413bf6a217027b35175ce75
Samuel Mwendwa <au791749@uni.au.dk>
2025-08-21 12:03:26 +0200

    more additions

--- tests/testthat/test_select.R
+++ tests/testthat/test_select.R
@@ -1,8 +1,7 @@
 test_that("my_select should work with variable indices", {
-  expect_error(my_select(as.matrix(iris), 1:3))
   expect_equal(dim(my_select(iris, 1:3)), c(nrow(iris), 3))
   expect_equal(dim(my_select(iris, -1)), c(nrow(iris), 4))
   expect_equal(dim(my_select(iris, 1)), c(nrow(iris), 1))
-  expect_s3_class(my_select(iris, 1), "dataframe")
+  expect_s3_class(my_select(iris, 1), "data.frame")
   expect_equal(2 * 2, 4)
 })
